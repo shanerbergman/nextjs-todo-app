45ee705f03d3c67ecef83b12c8d0c74584dee99e
Shane <shane.bergman@corteva.com>
2023-08-18 07:09:00 -0600

    add back new button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,12 @@ export default async function Home() {
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">Todos</h1>
-        {/*<Link
+        <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
           href="/new"
         >
           New
-  </Link>*/}
+        </Link>
       </header>
       <ul className="pl-4">
         {todos.map((todo) => (
